5ca8d82d1986c99db7739e4618b916788ce13d4d
Rommel Garay <a20200241@pucp.edu.pe>
2023-06-28 19:01:46 -0500

    Se corrigio Inicio sesion, mensaje

--- src/main/java/com/example/proyecto_iweb/models/daos/AdminJuegosDaos.java
+++ src/main/java/com/example/proyecto_iweb/models/daos/AdminJuegosDaos.java
@@ -87,7 +87,7 @@ public class AdminJuegosDaos  extends DaoBase{
         return lista;
     }
 
-    public void crearJuego(String nombre, String descripcion, double precio, int stock, String consola, String genero, InputStream file){
+    public void crearJuego(String nombre, String descripcion, double precio, int stock, String consola, String genero, byte[] foto){
 
         String sql = "INSERT INTO juego (nombre,descripcion,precio,descuento,stock,consola,genero,fotoJuego,existente,habilitado) VALUES (?,?,?,'0',?,?,?,?,'1','1')";
 
@@ -101,8 +101,8 @@ public class AdminJuegosDaos  extends DaoBase{
             pstmt.setString(5, consola);
             pstmt.setString(6, genero);
 
-            pstmt.setBlob(7, file);
-
+            //pstmt.setBlob(7, file);
+            pstmt.setBytes(7, foto);
             pstmt.executeUpdate();
 
 
